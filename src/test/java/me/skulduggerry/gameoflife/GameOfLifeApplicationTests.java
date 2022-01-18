dc370583338dc35b57c490e0f046c8e4f34a5467
Felix Kretke <felix.kretke@gbtec.com>
2022-01-18 16:23:32 +0100

    Initial commit

---
+++ src/test/java/me/skulduggerry/gameoflife/GameOfLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.skulduggerry.gameoflife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOfLifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
