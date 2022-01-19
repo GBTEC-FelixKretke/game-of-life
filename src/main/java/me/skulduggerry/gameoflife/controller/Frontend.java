1d99d957bfbba12ce0c91b5c3ef77a36f8ebf944
Felix Kretke <felix.kretke@gbtec.com>
2022-01-19 16:09:22 +0100

    refactor(DES-2329): remove unnecessary parameter

--- src/main/java/me/skulduggerry/gameoflife/controller/Frontend.java
+++ src/main/java/me/skulduggerry/gameoflife/controller/Frontend.java
@@ -1,14 +1,13 @@
 package me.skulduggerry.gameoflife.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Frontend {
 
     @GetMapping("/")
-    public String gameOfLife(Model model) {
+    public String gameOfLife() {
         return "index";
     }
 }
