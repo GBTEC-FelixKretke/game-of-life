1981b44aa2cb7649228fabd612707780cc4b4eaa
Felix Kretke <felix.kretke@gbtec.com>
2022-01-20 15:49:09 +0100

    feat(DES-2329): Create a class to store constants for the frontend

---
+++ src/main/java/me/skulduggerry/gameoflife/controller/ModelConstants.java
@@ -0,0 +1,10 @@
+package me.skulduggerry.gameoflife.controller;
+
+public class ModelConstants {
+
+    private ModelConstants() {
+    }
+
+    public static final String MESSAGE = "Upload a file.";
+    public static final String FILE = "file";
+}
