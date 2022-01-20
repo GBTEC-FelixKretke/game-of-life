772e7b8905d03f1d1f65f0c1de81df088997f5fd
Felix Kretke <felix.kretke@gbtec.com>
2022-01-20 12:18:52 +0100

    feat(DES-2329): Add message

--- src/main/java/me/skulduggerry/gameoflife/controller/Frontend.java
+++ src/main/java/me/skulduggerry/gameoflife/controller/Frontend.java
@@ -1,13 +1,15 @@
 package me.skulduggerry.gameoflife.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Frontend {
 
     @GetMapping("/")
-    public String gameOfLife() {
+    public String gameOfLife(Model model) {
+        model.addAttribute("message", "Lade eine Datei hoch");
         return "index";
     }
 }
