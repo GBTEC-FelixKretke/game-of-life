2b419a4b44955eec8c802447f6f342d47df1456a
Felix Kretke <felix.kretke@gbtec.com>
2022-01-25 14:31:37 +0100

    feat(DES-2330): Use singleton pattern for WorkGeneration

--- src/main/java/me/skulduggerry/gameoflife/model/WorkGeneration.java
+++ src/main/java/me/skulduggerry/gameoflife/model/WorkGeneration.java
@@ -1,7 +1,25 @@
 package me.skulduggerry.gameoflife.model;
 
-public record WorkGeneration(Dimension dimension, Cell[][] cells) {
+import static lombok.AccessLevel.PRIVATE;
 
-    public record Cell(Integer x, Integer y, Boolean alive) {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor(access = PRIVATE)
+public class WorkGeneration {
+
+    private Dimension dimension;
+    private Cell[][] cells;
+
+    private static WorkGeneration INSTANCE;
+
+    public static WorkGeneration getInstance() {
+        return INSTANCE != null ? INSTANCE : (INSTANCE = new WorkGeneration(null, null));
+    }
+
+    public record Cell(Boolean alive) {
     }
 }
