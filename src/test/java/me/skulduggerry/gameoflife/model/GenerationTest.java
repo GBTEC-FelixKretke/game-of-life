c2dd55ba45f2b5fff048d1e1d98928000f4f5333
Felix Kretke <felix.kretke@gbtec.com>
2022-01-27 10:56:29 +0100

    test(DES-2330): Add test for Generation

---
+++ src/test/java/me/skulduggerry/gameoflife/model/GenerationTest.java
@@ -0,0 +1,54 @@
+package me.skulduggerry.gameoflife.model;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import me.skulduggerry.gameoflife.exception.InvalidDataException;
+
+class GenerationTest {
+
+    @Test
+    void wrong_field_data_size() {
+        Dimension dimension = new Dimension(3, 3);
+        List<List<Integer>> fieldData = Arrays.asList(
+        // @formatter:off
+                Arrays.asList(0, 1, 0),
+                Arrays.asList(0, 1, 0)
+                // @formatter:on
+        );
+
+        assertThrows(InvalidDataException.class, () -> new Generation(dimension, fieldData));
+    }
+
+    @Test
+    void wrong_line_length() {
+        Dimension dimension = new Dimension(3, 3);
+        List<List<Integer>> fieldData = Arrays.asList(
+        // @formatter:off
+                Arrays.asList(0, 1, 0),
+                Arrays.asList(0, 1, 0),
+                Arrays.asList(0, 1, 0, 1)
+                // @formatter:on
+        );
+
+        assertThrows(InvalidDataException.class, () -> new Generation(dimension, fieldData));
+    }
+
+    @Test
+    void null_line() {
+        Dimension dimension = new Dimension(3, 3);
+        List<List<Integer>> fieldData = Arrays.asList(
+        // @formatter:off
+                Arrays.asList(0, 1, 0),
+                Arrays.asList(0, 1, 0),
+                null
+                // @formatter:on
+        );
+
+        assertThrows(InvalidDataException.class, () -> new Generation(dimension, fieldData));
+    }
+}
