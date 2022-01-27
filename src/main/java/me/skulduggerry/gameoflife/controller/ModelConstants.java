d0ff0f757cc28f5c6cb527183e948dfe44ebcfbc
Felix Kretke <felix.kretke@gbtec.com>
2022-01-27 13:50:28 +0100

    refactor(DES-2330): Put index texts in ModelConstants

--- src/main/java/me/skulduggerry/gameoflife/controller/ModelConstants.java
+++ src/main/java/me/skulduggerry/gameoflife/controller/ModelConstants.java
@@ -6,6 +6,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ModelConstants {
 
-    public static final String MESSAGE = "Upload a file.";
-    public static final String FILE = "file";
+    public static final String FILE_ID = "file";
+    public static final String TABLE_DIV_ID = "table_div";
+    public static final String TABLE_ID = "table";
+    public static final String UPLOAD_BUTTON_ID = "upload_button";
+    public static final String NEXT_GEN_BUTTON_ID = "next_gen_button";
+
+    public static final String UPLOAD_TITLE = "Upload a generation";
+    public static final String UPLOAD_MESSAGE = "Upload a file.";
+    public static final String UPLOAD_INVALID = "Uploaded file is invalid!";
+    public static final String UPLOAD_BUTTON = "Upload File";
+
+    public static final String NEXT_GEN_TITLE = "Calculate the next generation";
+    public static final String NEXT_GEN_BUTTON = "Next Generation";
+
 }
