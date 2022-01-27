680e4d6de6c8fa4ba320519535a6559817304751
Felix Kretke <felix.kretke@gbtec.com>
2022-01-27 10:52:14 +0100

    feat(DES-2330): Remove class which are no longer needed

--- src/main/java/me/skulduggerry/gameoflife/exceptions/InvalidDataException.java
+++
@@ -1,8 +0,0 @@
-package me.skulduggerry.gameoflife.exceptions;
-
-public class InvalidDataException extends IllegalArgumentException {
-
-    public InvalidDataException(String cause) {
-        super("Data is invalid because " + cause);
-    }
-}
