b44a470550abf9c9682bd5ebc14c940252f8a3ed
Felix Kretke <felix.kretke@gbtec.com>
2022-01-28 15:56:46 +0100

    feat(DES-2332): Add Generation Creator website

--- src/main/java/me/skulduggerry/gameoflife/controller/Binding.java
+++ src/main/java/me/skulduggerry/gameoflife/controller/Binding.java
@@ -7,10 +7,14 @@ import lombok.NoArgsConstructor;
 public final class Binding {
 
     public static final String ROOT = "/";
+    public static final String UI_PATH = ROOT + "ui/";
 
-    public static final String UPLOAD_PATH = ROOT + "upload";
-    public static final String CURRENT_GENERATION_PATH = ROOT + "currentGeneration";
-    public static final String NEXT_GENERATION_PATH = ROOT + "nextGeneration";
+    public static final String INDEX_PATH = UI_PATH;
+    public static final String GENERATION_CREATOR_PATH = UI_PATH + "generationCreator";
+    public static final String UPLOAD_PATH = UI_PATH + "upload";
+    public static final String CURRENT_GENERATION_PATH = UI_PATH + "currentGeneration";
+    public static final String NEXT_GENERATION_PATH = UI_PATH + "nextGeneration";
 
     public static final String INDEX_FORWARD = "index";
+    public static final String GENERATION_CREATOR_FORWARD = "generation_creator";
 }
--- src/main/java/me/skulduggerry/gameoflife/controller/Frontend.java
+++ src/main/java/me/skulduggerry/gameoflife/controller/Frontend.java
@@ -1,23 +1,40 @@
 package me.skulduggerry.gameoflife.controller;
 
 import static me.skulduggerry.gameoflife.controller.Binding.CURRENT_GENERATION_PATH;
+import static me.skulduggerry.gameoflife.controller.Binding.GENERATION_CREATOR_FORWARD;
+import static me.skulduggerry.gameoflife.controller.Binding.GENERATION_CREATOR_PATH;
 import static me.skulduggerry.gameoflife.controller.Binding.INDEX_FORWARD;
+import static me.skulduggerry.gameoflife.controller.Binding.INDEX_PATH;
 import static me.skulduggerry.gameoflife.controller.Binding.NEXT_GENERATION_PATH;
 import static me.skulduggerry.gameoflife.controller.Binding.ROOT;
+import static me.skulduggerry.gameoflife.controller.Binding.UI_PATH;
 import static me.skulduggerry.gameoflife.controller.Binding.UPLOAD_PATH;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class Frontend {
 
     @GetMapping(ROOT)
+    public RedirectView root() {
+        return new RedirectView(UI_PATH);
+    }
+
+    @GetMapping(INDEX_PATH)
     public String gameOfLife(Model model) {
         model.addAttribute("current_gen_endpoint", CURRENT_GENERATION_PATH);
         model.addAttribute("next_gen_endpoint", NEXT_GENERATION_PATH);
         model.addAttribute("upload_endpoint", UPLOAD_PATH);
+        model.addAttribute("generation_creator", GENERATION_CREATOR_PATH);
         return INDEX_FORWARD;
+    }
+
+    @GetMapping(GENERATION_CREATOR_PATH)
+    public String generationCreator(Model model) {
+        model.addAttribute("index", INDEX_PATH);
+        return GENERATION_CREATOR_FORWARD;
     }
 }
