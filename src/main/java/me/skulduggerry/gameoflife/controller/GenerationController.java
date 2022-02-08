9e8ea288f923a3c2777bcfab716471af8ae33e36
Felix Kretke <f.kretke@gmail.com>
2022-02-08 17:48:10 +0100

    refactor(DES-2332): Remove unused attribute

--- src/main/java/me/skulduggerry/gameoflife/controller/GenerationController.java
+++ src/main/java/me/skulduggerry/gameoflife/controller/GenerationController.java
@@ -1,29 +1,24 @@
 package me.skulduggerry.gameoflife.controller;
 
-import static me.skulduggerry.gameoflife.controller.Binding.CURRENT_GENERATION_PATH;
-import static me.skulduggerry.gameoflife.controller.Binding.NEXT_GENERATION_PATH;
-import static me.skulduggerry.gameoflife.controller.Binding.UPLOAD_PATH;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.skulduggerry.gameoflife.model.Generation;
 import me.skulduggerry.gameoflife.service.GenerationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import static me.skulduggerry.gameoflife.controller.Binding.CURRENT_GENERATION_PATH;
+import static me.skulduggerry.gameoflife.controller.Binding.NEXT_GENERATION_PATH;
+import static me.skulduggerry.gameoflife.controller.Binding.UPLOAD_PATH;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
 public class GenerationController {
 
-    @NonNull
-    private final ObjectMapper mapper;
     @NonNull
     private final GenerationService generationService;
 
